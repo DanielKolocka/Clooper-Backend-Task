f8902beb98e6cc34d7bdd295dbd2fa6307d90e15
Daniel Kolocka <dkolocka@sfu.ca>
2023-01-04 18:23:28 +0100

    Update property route added

--- controllers/propertiesController.js
+++ controllers/propertiesController.js
@@ -12,3 +12,33 @@ exports.newProperty = async (req, res, next) => {
     });
 
 }
+
+// Update a property => /properties/:id
+exports.updateProperty = async (req, res, next) => {
+    const propertyId = req.params.id;
+    let property = await Property.findById(propertyId);
+
+    if (!property) {
+        res.status(404).json({
+            success: false,
+            message: 'Property not found.'
+        });
+    }
+
+    // Update the property if found
+    property = await Property.findByIdAndUpdate(propertyId, req.body, {
+        new: true
+    });
+
+    res.status(200).json({
+        success: true,
+        message: 'Property is updated,',
+        data: property
+    });
+}
+
+// Delete a property => /properties/:id
+
+// Publish a property => /properties/publish/:id
+
+// Get property by address => /properties/:address
--- routes/properties.js
+++ routes/properties.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // Import Methods
-const { newProperty } = require('../controllers/propertiesController');
+const { newProperty, updateProperty } = require('../controllers/propertiesController');
 
 // router.route('').get();
 
 router.route('/properties/new').post(newProperty);
+router.route('/properties/:id').post(updateProperty);
 
 // router.route('').put();
 
